31deab1d8894797c9c2cb13fcdcff4cb7f90d00e
kshick <36218001+kshick@users.noreply.github.com>
2018-02-07 23:11:51 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -19,34 +19,45 @@ angular.module('myApp', [])
     var cities = this;
     
     cities.drawPile = [
-    	{text:'Atlanta', done:false, color:"blue", count:1, max:1},
-    	{text:'Chicago', done:false, color:"blue", count:2, max:2},
-    	{text:'Denver', done:false, color:"blue", count:2, max:2},
-    	{text:'London', done:false, color:"blue", count:3, max:3},
-    	{text:'New York', done:false, color:"blue", count:3, max:3},
-    	{text:'Washington', done:false, color:"blue", count:3, max:3},
-	    {text:'Buenos Aires', done:false, color:"yellow", count:2, max:2},
-	    {text:'Jacksonville', done:false, color:"yellow", count:3, max:3},
-	    {text:'Mexico City', done:false, color:"yellow", count:1, max:1},
-	    {text:'Lima', done:false, color:"yellow", count:1, max:1},
-	    {text:'Lagos', done:false, color:"yellow", count:3, max:3},
-	    {text:'Sao Paulo', done:false, color:"yellow", count:3, max:3},
-	    {text:'Cairo', done:false, color:"black", count:3, max:3},
-	    {text:'Istanbul', done:false, color:"black", count:3, max:3},
-	    {text:'Tripoli', done:false, color:"black", count:3, max:3}];
+    	{text:'Atlanta', color:"blue", count:1, max:1},
+    	{text:'Chicago', color:"blue", count:2, max:2},
+    	{text:'Denver', color:"blue", count:2, max:2},
+    	{text:'London', color:"blue", count:3, max:3},
+    	{text:'New York', color:"blue", count:3, max:3},
+    	{text:'Washington',  color:"blue", count:3, max:3},
+	    {text:'Buenos Aires', color:"yellow", count:2, max:2},
+	    {text:'Jacksonville', color:"yellow", count:3, max:3},
+	    {text:'Mexico City', color:"yellow", count:1, max:1},
+	    {text:'Lima',  color:"yellow", count:1, max:1},
+	    {text:'Lagos', color:"yellow", count:3, max:3},
+	    {text:'Sao Paulo', color:"yellow", count:3, max:3},
+	    {text:'Cairo', color:"black", count:3, max:3},
+	    {text:'Istanbul', color:"black", count:3, max:3},
+	    {text:'Tripoli', color:"black", count:3, max:3}];
  
     //  Epidemic progression - 2,2,2,3,3,4,4,5
-    // ToDo: Add admin menu for each city with abilit to remove from game or manually move between groups
-    // ToDo: add stats / percentages
-    // ToDo: make pretty
+
     
     cities.discardPile = [];
     
-    cities.addNewCity = function() {
-      cities.drawPile.push({text:cities.newCity, done:false});
-      cities.newCity = '';
+    cities.adminMode = false;
+    
+    cities.getDrawPileCursor = function() {
+    	if(cities.redrawStack.length > 0) {
+    		return "default";
+    	}
+    	
+    	return "pointer";
     };
- 
+    
+    cities.getReDrawPileCursor = function(cityGroup) {
+    	if(cities.isTopGroup(cityGroup)){
+    		return "pointer";
+    	}
+    	
+    	return "default";
+    };
+    
     cities.remainingDraw = function() {
       var i = 0;
       angular.forEach(cities.drawPile, function(city) {
@@ -88,8 +99,9 @@ angular.module('myApp', [])
       };
     
       cities.isTopGroup = function(cityGroup) {
-    	var index = cities.redrawStack.indexOf(cityGroup);
-    	return index == 0;
+    	  
+    	var index = cities.redrawStack.indexOf(cityGroup);  	
+    	return index == 0;	
     	
       };
       
@@ -110,4 +122,50 @@ angular.module('myApp', [])
     	cities.selectedCity=" ";
     	cities.epidemicButtonDisabled = true;
     };
+    
+    //Admin functions
+    
+    cities.adminText = "Enable";
+    
+    cities.toggleAdminMode = function() {
+    	
+    	cities.adminMode = !cities.adminMode;
+    	
+    	if(cities.adminMode){
+    		cities.adminText = "Disable";
+    	} else {
+    		cities.adminText = "Enable";
+    	}
+    	
+    };
+    
+    cities.removeFromDraw = function(city) {
+    	if(confirm("Remove " + city.text + "?")){
+    		var index = cities.drawPile.indexOf(city);
+    		cities.drawPile.splice(index, 1);
+    	}
+    };
+    
+    cities.removeFromRedraw = function(cityGroup, city) {
+    	if(confirm("Remove " + city.text + "?")){
+    		var index = cityGroup.indexOf(city);
+      	  	cityGroup.splice(index, 1);
+    	}
+    };
+    
+    cities.removeFromDiscard = function(city) {
+    	if(confirm("Remove " + city.text + "?")){
+    		var index = cities.discardPile.indexOf(city);
+    		cities.discardPile.splice(index, 1);
+    	}
+    };
+    
+    cities.increaseCityCount = function(city) {
+    	city.count = city.count + 1;
+    }
+    
+    cities.decreaseCityCount = function(city) {
+    	city.count = city.count -1;
+    }
+    
   });
