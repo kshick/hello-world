1d36f9ea98794b91970ed0faf5b11f2701c81721
mwilkens731 <matthew.j.wilkens@gmail.com>
2018-04-19 15:59:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ angular.module('myApp', [])
     cities.drawPile = [
 			{text:'Hollow Men Gather', color:"green", count:0, max:4},
     	{text:'Atlanta', color:"blue", count:1, max:1},
-    	{text:'Chicago', color:"blue", count:1, max:1}, //max:2
+    	//{text:'Chicago', color:"blue", count:1, max:1}, //max:2
     	{text:'Denver', color:"blue", count:1, max:1}, //max:2
     	{text:'Frankfurt', color:"blue", count:2, max:2},
 	{text:'Johannesburg', color:"blue", count:1, max:1}, //max:2
@@ -32,8 +32,9 @@ angular.module('myApp', [])
 	{text:'Paris', color:"blue", count:2, max:2},
 	{text:'San Francisco', color:"blue", count:1, max:1}, //max:2
     	{text:'Washington',  color:"blue", count:2, max:2}, //max:3
-	//{text:'Bogota', color:"yellow", count:0, max:0}, //max:2
+	{text:'Bogota', color:"yellow", count:1, max:1}, //max:2
 	{text:'Buenos Aires', color:"yellow", count:2, max:2},
+	{text:'Dar es Salaam', color:"yellow", count:2, max:2},
 	//{text:'Jacksonville', color:"yellow", count:0, max:0}, //max:3
   {text:'Khartoum', color:"yellow", count:1, max:1},
 	{text:'Kinshasa', color:"yellow", count:1, max:1},
@@ -47,6 +48,7 @@ angular.module('myApp', [])
 	{text:'Baghdad', color:"black", count:2, max:2},
 	{text:'Cairo', color:"black", count:1, max:1}, //max:3
 	{text:'Istanbul', color:"black", count:3, max:3},
+	{text:'Kolkata', color:"black", count:1, max:1},
 	{text:'Moscow', color:"black", count:1, max:1},
 	{text:'New Mumbai', color:"black", count:2, max:2},
 	{text:'Riyadh', color:"black", count:2, max:2},
